aab489aa91b238cd8e566198e3b7a3be3d9954a6
Luis Cruz <luisccc99@gmail.com>
2022-03-27 04:00:21 -0700

    Change database url for production deployment

---
+++ config/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  development: {
+    username: "admin",
+    password: "PM6fA7DjnpQc",
+    database: "asistencias",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    port: "55432"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    use_env_variable: process.env.DATABASE_URL
+  }
+}
