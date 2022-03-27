c28283bb2871bcb6a797e7736c37d55eaf9a85c1
Luis Cruz <luisccc99@gmail.com>
2022-03-27 04:39:30 -0700

    minor changes

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    "use_env_variable": process.env.DATABASE_URL,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
